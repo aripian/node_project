8e3d17c9210bc53e4837e645f98d6b62251a133c
brightclicksolution <arifZakaria@brightclick.com.my>
2016-09-07 09:11:56 +0800

    Initial Commit

---
+++ node_exp/server.js
@@ -0,0 +1,21 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+var colors = require('colors');
+var port = process.env.port || 3000;
+
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/src', express.static(__dirname + '/src'));
+app.use('/views', express.static(__dirname + '/views'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html"); 
+});
+
+app.listen(port, function () {
+    console.log(('Example app listening on port: ' + port).rainbow);
+});
---
+++ node_exp/src/app.js
@@ -0,0 +1,53 @@
+﻿var nodeApp = angular.module('nodeApp', ['ngRoute']);
+
+// configure our routes
+nodeApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'mainController'
+        })
+
+        // route for the about page
+        .when('/charts', {
+            templateUrl: 'views/charts.html',
+            controller: 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/tables', {
+            templateUrl: 'views/tables.html',
+            controller: 'contactController'
+        })
+
+        .when('/forms', {
+            templateUrl: 'views/forms.html',
+            controller: 'contactController'
+        })
+
+        .when('/bootstrapsgrid', {
+            templateUrl: 'views/bootstrapsgrid.html',
+            controller: 'contactController'
+        })
+
+        .when('/bootstrapselement', {
+            templateUrl: 'views/bootstrapselement.html',
+            controller: 'contactController'
+        });;
+});
+
+// create the controller and inject Angular's $scope
+nodeApp.controller('mainController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+nodeApp.controller('aboutController', function ($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+nodeApp.controller('contactController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
